0bab2c1597b1f96051b4ba639a3198260c3d4218
Togrul Asadov <adsads00123@gmail.com>
2023-10-15 10:40:25 +0400

    removed unnecessary views

--- src/presentation/urls.py
+++ src/presentation/urls.py
@@ -7,12 +7,6 @@ app_name = "presentation"
 urlpatterns = [
     path("", views.HomeView.as_view(), name="home"),
     path("about-us", views.AboutUs.as_view(), name="about-us"),
-    path("services", views.ServiceView.as_view(), name="service"),
-    path("services/<int:pk>", views.ServiceDetailView.as_view(), name="service-detail"),
-    # path("services/<slug:slug>", views.ServiceDetailView.as_view(), name="service-detail-slug"),
-    path("blogs", views.BlogView.as_view(), name="blog"),
-    path("blogs/<slug:slug>", views.BlogDetailView.as_view(), name="blog-detail"),
-    path("pricing", views.Pricing.as_view(), name="pricing"),
     path("faq", views.FAQView.as_view(), name="faq"),
     path("contact", views.ContactView.as_view(), name="contact"),
 ]
--- src/presentation/views.py
+++ src/presentation/views.py
@@ -1,7 +1,5 @@
 from django.views.generic import TemplateView
 
-# Create your views here.
-
 
 class HomeView(TemplateView):
     """View to render homepage."""
@@ -11,26 +9,6 @@ class HomeView(TemplateView):
 
 class AboutUs(TemplateView):
     template_name = "presentation/about.html"
-
-
-class ServiceView(TemplateView):
-    template_name = "presentation/service.html"
-
-
-class ServiceDetailView(TemplateView):
-    template_name = "presentation/service-details.html"
-
-
-class BlogView(TemplateView):
-    template_name = "presentation/blog.html"
-
-
-class BlogDetailView(TemplateView):
-    template_name = "presentation/blog-details.html"
-
-
-class Pricing(TemplateView):
-    template_name = "presentation/pricing.html"
 
 
 class FAQView(TemplateView):
